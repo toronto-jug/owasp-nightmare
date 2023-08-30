1f4a48a466274991ea9d835edbfc3cdaf7e2a657
Max Barkley <max@dnastack.com>
2023-08-30 15:59:29 -0400

    Added custom user details manager

---
+++ src/main/java/ca/tjug/owaspnightmare/BadUserDetailsManager.java
@@ -0,0 +1,42 @@
+package ca.tjug.owaspnightmare;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BadUserDetailsManager implements UserDetailsManager {
+    @Override
+    public void createUser(UserDetails user) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return true;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User(username, "{noop}" + username, List.of());
+    }
+}
--- src/main/java/ca/tjug/owaspnightmare/SecurityConfiguration.java
+++ src/main/java/ca/tjug/owaspnightmare/SecurityConfiguration.java
@@ -20,9 +20,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -48,17 +45,5 @@ public class SecurityConfiguration {
 		// @formatter:on
 		return http.build();
 	}
-
-	// @formatter:off
-	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(user);
-	}
-	// @formatter:on
 
 }
