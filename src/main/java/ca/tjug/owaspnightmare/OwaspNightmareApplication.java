910699ff62c48f65a23197805fad949f37196273
Max Barkley <max@dnastack.com>
2023-08-30 11:11:10 -0400

    Initial commit

---
+++ src/main/java/ca/tjug/owaspnightmare/OwaspNightmareApplication.java
@@ -0,0 +1,13 @@
+package ca.tjug.owaspnightmare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwaspNightmareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwaspNightmareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/tjug/owaspnightmare/OwaspNightmareApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tjug.owaspnightmare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwaspNightmareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
